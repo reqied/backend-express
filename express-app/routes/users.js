e33ec8943108f5965bee8babee8e1e14067ffa63
Arina Kiskina <ae.kiskina@gmail.com>
2026-03-24 18:51:16 +0500

    3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -2,8 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
+const obj1 = {
+  "id": 1,
+  "name": "Кискина Арина"
+}
+const obj2 = {
+  "id": 2,
+  "name": "Черезов Тимофей"
+}
+
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send([obj1, obj2]);
 });
 
 module.exports = router;
